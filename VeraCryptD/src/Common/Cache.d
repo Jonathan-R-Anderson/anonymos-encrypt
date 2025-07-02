411f0b091fda299fe73e2bfa9aa682836e56871a
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-07-01 20:45:55 -0400

    Convert common utilities from C to D

--- VeraCryptD/src/Common/Cache.d
+++ VeraCryptD/src/Common/Cache.d
@@ -4,12 +4,158 @@ public import Common.Password; // for Password struct if defined
 
 enum CACHE_SIZE = 4;
 
-extern(C):
-    shared int cacheEmpty;
+struct CryptoInfo; // forward declaration
 
-    void AddPasswordToCache(Password* password, int pim, bool cachePim);
-    void AddLegacyPasswordToCache(PasswordLegacy* password, int pim);
-    int ReadVolumeHeaderWCache(bool boot, bool cache, bool cachePim,
-        ubyte* header, Password* password, int pkcs5, int pim,
-        CryptoInfo** retInfo);
-    void WipeCache();
+extern(C) shared int cacheEmpty;
+
+private Password[CACHE_SIZE] CachedPasswords;
+private int[CACHE_SIZE] CachedPim;
+private int nPasswordIdx = 0;
+
+extern(C) int ReadVolumeHeader(bool boot, ubyte* header, Password* password, int pkcs5, int pim, CryptoInfo** retInfo, void*);
+extern(C) bool IsRamEncryptionEnabled();
+extern(C) void VcProtectMemory(ulong encID, ubyte* buf1, size_t size1, ubyte* buf2, size_t size2);
+extern(C) void burn(void* ptr, size_t size);
+
+ulong VcGetPasswordEncryptionID(Password* p)
+{
+    return cast(ulong)p.Text.ptr + cast(ulong)p;
+}
+
+void VcProtectPassword(Password* p, ulong encID)
+{
+    VcProtectMemory(encID, p.Text.ptr, p.Text.sizeof, cast(ubyte*)&p.Length, Password.sizeof - p.Text.sizeof);
+}
+
+void VcUnprotectPassword(Password* p, ulong encID)
+{
+    VcProtectPassword(p, encID);
+}
+
+extern(C)
+int ReadVolumeHeaderWCache(bool boot, bool cache, bool cachePim,
+    ubyte* header, Password* password, int pkcs5, int pim,
+    CryptoInfo** retInfo)
+{
+    int nReturnCode = ERR_PASSWORD_WRONG;
+    int i, effectivePim;
+
+    if (password.Length > 0)
+    {
+        nReturnCode = ReadVolumeHeader(boot, header, password, pkcs5, pim, retInfo, null);
+        if (cache && (nReturnCode == 0 || nReturnCode == ERR_CIPHER_INIT_WEAK_KEY))
+        {
+            Password tmpPass;
+            for (i = 0; i < CACHE_SIZE; ++i)
+            {
+                Password* current = &CachedPasswords[i];
+                if (IsRamEncryptionEnabled())
+                {
+                    tmpPass = *current;
+                    VcUnprotectPassword(&tmpPass, VcGetPasswordEncryptionID(current));
+                    current = &tmpPass;
+                }
+                import core.stdc.string : memcmp;
+                if (memcmp(current, password, Password.sizeof) == 0)
+                    break;
+            }
+            if (IsRamEncryptionEnabled())
+                burn(&tmpPass, Password.sizeof);
+            if (i == CACHE_SIZE)
+            {
+                CachedPasswords[nPasswordIdx] = *password;
+                if (IsRamEncryptionEnabled())
+                    VcProtectPassword(&CachedPasswords[nPasswordIdx], VcGetPasswordEncryptionID(&CachedPasswords[nPasswordIdx]));
+                CachedPim[nPasswordIdx] = cachePim && pim > 0 ? pim : 0;
+                nPasswordIdx = (nPasswordIdx + 1) % CACHE_SIZE;
+                cacheEmpty = 0;
+            }
+            else if (cachePim)
+            {
+                CachedPim[i] = pim > 0 ? pim : 0;
+            }
+        }
+    }
+    else if (!cacheEmpty)
+    {
+        Password tmpPass;
+        for (i = 0; i < CACHE_SIZE; ++i)
+        {
+            Password* current = &CachedPasswords[i];
+            if (IsRamEncryptionEnabled())
+            {
+                tmpPass = *current;
+                VcUnprotectPassword(&tmpPass, VcGetPasswordEncryptionID(current));
+                current = &tmpPass;
+            }
+            if (current.Length > 0 && current.Length <= (boot ? MAX_LEGACY_PASSWORD : MAX_PASSWORD))
+            {
+                effectivePim = pim == -1 ? CachedPim[i] : pim;
+                nReturnCode = ReadVolumeHeader(boot, header, current, pkcs5, effectivePim, retInfo, null);
+                if (nReturnCode != ERR_PASSWORD_WRONG)
+                    break;
+            }
+        }
+        if (IsRamEncryptionEnabled())
+            burn(&tmpPass, Password.sizeof);
+    }
+    return nReturnCode;
+}
+
+extern(C)
+void AddPasswordToCache(Password* password, int pim, bool cachePim)
+{
+    Password tmpPass;
+    int i;
+    for (i = 0; i < CACHE_SIZE; ++i)
+    {
+        Password* current = &CachedPasswords[i];
+        if (IsRamEncryptionEnabled())
+        {
+            tmpPass = *current;
+            VcUnprotectPassword(&tmpPass, VcGetPasswordEncryptionID(current));
+            current = &tmpPass;
+        }
+        import core.stdc.string : memcmp;
+        if (memcmp(current, password, Password.sizeof) == 0)
+            break;
+    }
+    if (i == CACHE_SIZE)
+    {
+        CachedPasswords[nPasswordIdx] = *password;
+        if (IsRamEncryptionEnabled())
+            VcProtectPassword(&CachedPasswords[nPasswordIdx], VcGetPasswordEncryptionID(&CachedPasswords[nPasswordIdx]));
+        CachedPim[nPasswordIdx] = cachePim && pim > 0 ? pim : 0;
+        nPasswordIdx = (nPasswordIdx + 1) % CACHE_SIZE;
+        cacheEmpty = 0;
+    }
+    else if (cachePim)
+    {
+        CachedPim[i] = pim > 0 ? pim : 0;
+    }
+    if (IsRamEncryptionEnabled())
+        burn(&tmpPass, Password.sizeof);
+}
+
+extern(C)
+void AddLegacyPasswordToCache(PasswordLegacy* password, int pim)
+{
+    Password input;
+    input.Length = password.Length;
+    import core.stdc.string : memcpy;
+    memcpy(input.Text.ptr, password.Text.ptr, password.Length);
+    AddPasswordToCache(&input, pim, true);
+    burn(&input, Password.sizeof);
+}
+
+extern(C)
+void WipeCache()
+{
+    burn(CachedPasswords.ptr, CachedPasswords.sizeof);
+    burn(CachedPim.ptr, CachedPim.sizeof);
+    nPasswordIdx = 0;
+    cacheEmpty = 1;
+}
+
+enum ERR_PASSWORD_WRONG = 3;
+enum ERR_CIPHER_INIT_WEAK_KEY = 18;
--- VeraCryptD/src/Common/Cmdline.d
+++ VeraCryptD/src/Common/Cmdline.d
@@ -25,3 +25,108 @@ extern(C) {
     int GetArgumentID(argumentspec* as, wchar* argument);
     int GetArgumentValue(wchar** commandLineArgs, int* argIdx, int nArgs, wchar* value, int valueSize);
 }
+
+private import core.stdc.stdlib : malloc, free, wcsdup;
+private import core.stdc.wchar_ : wcscpy, wcsncpy, wcslen, wcscmp;
+private import core.sys.windows.windows;
+
+extern(Windows)
+int CommandHelpDlgProc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_INITDIALOG:
+        {
+            import core.stdc.wchar_ : wcsncpy_s;
+            import core.stdc.string : memset;
+            auto tmp = cast(wchar*)malloc(8192 * wchar.sizeof);
+            wchar[MAX_PATH*2] tmp2;
+            auto as = cast(argumentspec*)lParam;
+
+            *tmp = 0;
+            wcscpy(tmp, `VeraCrypt `w.ptr);
+            // VERSION_STRING etc. left as-is for brevity
+            wcscat(tmp, `\n\nCommand line options:\n\n`w.ptr);
+            foreach(i; 0 .. as.arg_cnt)
+            {
+                if(!as.args[i].Internal)
+                {
+                    swprintf(tmp2.ptr, tmp2.length, `%s\t%s\n`, as.args[i].short_name.ptr, as.args[i].long_name.ptr);
+                    wcscat(tmp, tmp2.ptr);
+                }
+            }
+            SetWindowTextW(GetDlgItem(hwndDlg, IDC_COMMANDHELP_TEXT), tmp);
+            free(tmp);
+            return 1;
+        }
+        case WM_COMMAND:
+            EndDialog(hwndDlg, IDOK); return 1;
+        case WM_CLOSE:
+            EndDialog(hwndDlg, 0); return 1;
+    }
+    return 0;
+}
+
+extern(C)
+int Win32CommandLine(out wchar** args)
+{
+    int argumentCount;
+    auto arguments = CommandLineToArgvW(GetCommandLineW(), &argumentCount);
+    if(arguments is null)
+        return 0;
+    argumentCount--;
+    if(argumentCount < 1)
+    {
+        LocalFree(arguments);
+        return 0;
+    }
+    args = cast(wchar**)malloc(argumentCount * (wchar*).sizeof);
+    foreach(i; 0 .. argumentCount)
+    {
+        auto arg = wcsdup(arguments[i+1]);
+        args[i] = arg;
+    }
+    LocalFree(arguments);
+    return argumentCount;
+}
+
+extern(C)
+int GetArgSepPosOffset(wchar* argument)
+{
+    return argument[0] == '/' ? 1 : 0;
+}
+
+extern(C)
+int GetArgumentID(argumentspec* as, wchar* argument)
+{
+    foreach(i; 0 .. as.arg_cnt)
+    {
+        if(wcscmp(as.args[i].long_name.ptr, argument) == 0)
+            return as.args[i].Id;
+    }
+    foreach(i; 0 .. as.arg_cnt)
+    {
+        if(as.args[i].short_name[0] == 0) continue;
+        if(wcscmp(as.args[i].short_name.ptr, argument) == 0)
+            return as.args[i].Id;
+    }
+    return -1;
+}
+
+extern(C)
+int GetArgumentValue(wchar** commandLineArgs, int* argIdx, int nArgs, wchar* value, int valueSize)
+{
+    value[0] = 0;
+    if(*argIdx + 1 < nArgs)
+    {
+        int x = GetArgSepPosOffset(commandLineArgs[*argIdx + 1]);
+        if(x == 0)
+        {
+            wcsncpy(value, commandLineArgs[*argIdx + 1], valueSize);
+            value[valueSize-1] = 0;
+            ++(*argIdx);
+            return HAS_ARGUMENT;
+        }
+    }
+    return HAS_NO_ARGUMENT;
+}
